50293e3bb152bbdaeb00936f9458b4a13857bb70
juandavidcruzgomez <jdcruz-gomez@umi.us>
2024-01-27 23:03:53 +0100

    Forget to remove some code...

--- app/src/ml_tasks/steps/evaluate.py
+++ app/src/ml_tasks/steps/evaluate.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from sklearn.metrics import accuracy_score
 
-from .helper import load_data, load_one_image
+from .helper import load_data
 
 def start_evaluation(repo_path, model_path, metrics_path):
     test_csv_path = repo_path / "prepared/test.csv"
@@ -17,18 +17,6 @@ def start_evaluation(repo_path, model_path, metrics_path):
     accuracy_path = metrics_path / "accuracy.json"
     accuracy_path.write_text(json.dumps(metrics))
 
-def test_one_image(image_path, model_path):
-    test_data = load_one_image(image_path)
-    model = load(model_path / "model.joblib")
-    predictions = model.predict(test_data.reshape(1, -1))
-    print(predictions)
-    return predictions
-
 if __name__ == "__main__":
-    #repo_path = Path(__file__).parent.parent
-    #start_evaluation(repo_path)
-    MODEL_PATH = "models"
-    model_path = Path(str(Path(__file__).parent.parent.parent.parent) + "/" + MODEL_PATH)
-    image_path = "/Users/juandavidcruzgomez/Documents/perso/IMT/CursoIntersemestre2024/Tests/n03445777_70.JPEG"
-    image = Path(image_path)
-    test_one_image(image, model_path)
+    repo_path = Path(__file__).parent.parent
+    start_evaluation(repo_path)
